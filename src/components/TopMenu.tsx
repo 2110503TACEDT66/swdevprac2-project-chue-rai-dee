1733449eb60308e6312026fedfa18883bdf29e75
Pittayaice <pittayakhunpitak14@gmail.com>
2024-03-23 12:47:20 +0700

    Add signin

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,10 +1,11 @@
 import TopMenuItem from './TopMenuItem';
 import Image from 'next/image';
 import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { Link } from '@mui/material';
 
 export default async function TopMenu() {
-  // const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions)
   return (
     <div className="bg-white fixed top-0 left-0 right-0 z-30 border-b border-gray-300 flex items-center justify-between">
       <div className="flex items-center">
@@ -23,7 +24,7 @@ export default async function TopMenu() {
         </div>
       </div>
       <div className="flex items-center">
-        {/* {session ? (
+        {session ? (
           <Link href="/api/auth/signout">
             <div className="flex items-center h-full px-2 text-cyan-600 text-sm">
               Sign-Out of {session.user?.name}
@@ -35,7 +36,7 @@ export default async function TopMenu() {
               Sign-In
             </div>
           </Link>
-        )} */}
+        )}
       </div>
     </div>
   );
