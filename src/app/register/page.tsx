d6c032414f87238adee592e19a6210bab7a9ec04
Pittayaice <pittayakhunpitak14@gmail.com>
2024-03-24 14:10:07 +0700

    Add css register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,10 @@
-import { getServerSession } from "next-auth"
 import User from "@/db/model/User"
 import { dbConnect } from "@/db/dbConnect"
-import { revalidateTag } from "next/cache"
-import { redirect } from "next/navigation"
-import { signIn } from 'next-auth/react';
 import bcrypt from 'bcryptjs';
+import { redirect } from "next/navigation";
+import { revalidateTag } from "next/cache";
 
 export default async function RegisterPage(){
-
     const addUser = async (addUserForm:FormData) => {
         "use server"
         const name = addUserForm.get("name")
@@ -26,52 +23,40 @@ export default async function RegisterPage(){
                 "role": "user",
                 "tel": tel
             })
-
-            if (typeof window !== 'undefined') {
-                const result = await signIn('credentials', {
-                    redirect: false,
-                    email: email,
-                    password: password,
-                });
-    
-                if (result?.ok) {
-                    redirect('/'); 
-                } else {
-                    console.error("Sign-in failed after registration.");
-                }
-            }
         }catch(error){
             console.log(error)
         }
+        revalidateTag("users")
+        redirect("/api/auth/signin")
     }
 
     return(
-        <main className = 'bg-slate-100 m-10 p-10'>            
-            <form action={addUser}>
-                <div className="text-xl text-blue-700"> Register </div>
-                <div className = 'flex items-center w-1/2 my-2'>
-                    <label className = 'w-auto block text-gray-700 pr-4' htmlFor='name'>Name</label>
-                    <input type = 'text' required id='name' name='name' placeholder = 'Name' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
-                </div>
+        <main className = 'bg-gray-100 min-h-screen flex items-center justify-center'>            
+            <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+            <h1 className="text-3xl text-center text-gray-800 mb-6">Register</h1>
+                <form action={addUser} className="space-y-4">
+                    
+                        <label className = 'w-auto block text-gray-700 pr-4' htmlFor='name'>Name</label>
+                        <input type = 'text' required id='name' name='name' placeholder = 'Name' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
 
-                <div className = 'flex items-center w-1/2 my-2'>
-                    <label className = 'w-auto block text-gray-700 pr-4' htmlFor='email'>Email</label>
-                    <input type = 'text' required id='email' name='email' placeholder = 'Email' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
-                </div>
-                
-                <div className = 'flex items-center w-1/2 my-2'>
-                    <label className = 'w-auto block text-gray-700 pr-4' htmlFor='password'>Password</label>
-                    <input type = 'text' required id='password' name='password' placeholder = 'password' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
-                </div>
 
-                <div className = 'flex items-center w-1/2 my-2'>
-                    <label className = 'w-auto block text-gray-700 pr-4' htmlFor='tel'>Tel</label>
-                    <input type = 'text' required id='tel' name='tel' placeholder = 'tel' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
-                </div>
+                    
+                        <label className = 'w-auto block text-gray-700 pr-4' htmlFor='email'>Email</label>
+                        <input type = 'text' required id='email' name='email' placeholder = 'Email' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
 
-                <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white p-2 rounded' >Register</button>
-            </form>
+                    
+                    
+                        <label className = 'w-auto block text-gray-700 pr-4' htmlFor='password'>Password</label>
+                        <input type = 'text' required id='password' name='password' placeholder = 'password' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
 
+
+                    
+                        <label className = 'w-auto block text-gray-700 pr-4' htmlFor='tel'>Tel</label>
+                        <input type = 'text' required id='tel' name='tel' placeholder = 'tel' className = 'bg-white border-2 border-gray-200 rounded w-full p-2 text-gray-700 focus:outline-none focus:border-blue-400' />
+
+                    <button type='submit' className='bg-blue-500 hover:bg-blue-700 text-white p-2 rounded' >Register</button>
+                </form>
+            </div>
         </main>
     )
 }
