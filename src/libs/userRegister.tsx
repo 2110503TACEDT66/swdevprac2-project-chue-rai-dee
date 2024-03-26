e6dae3826d852658f7d3d3e83f2dde136e767a59
Itttikorn <ittikornsd@gmail.com>
2024-03-26 22:27:16 +0700

    use backend for register

--- src/libs/userRegister.tsx
+++ src/libs/userRegister.tsx
@@ -1,15 +1,16 @@
 export default async function userRegister(userName:string, userEmail: string, userPassword: string, userTel: string) {
     const response = await fetch('https://hotel-reservation-api-phi.vercel.app/api/v1/auth/register', {
       method: "POST",
+      cache: 'no-store',
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ name:userName, email: userEmail, password: userPassword, role:"user", tel: userTel }),
     });
   
-    if (!response.ok) {
-      throw new Error("Failed to register");
-    }
+    // if (!response.ok) {
+    //   throw new Error("Failed to register");
+    // }
   
     return await response.json();
   }
